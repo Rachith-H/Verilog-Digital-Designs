5eadb733e2ec782e21fd0b78bd6987766b4102c2
Rachith-H <157898134+Rachith-H@users.noreply.github.com>
2025-10-23 19:55:08 +0530

    Update decoder_2to4.v

--- Decoders/Decoder_2to4/decoder_2to4.v
+++ Decoders/Decoder_2to4/decoder_2to4.v
@@ -1,11 +1,11 @@
 `timescale 1ns / 1ps
-module decoder_2to4 (in0,in1,out0,out1,out2,out3);
-input in0,in1;
+module decoder_2to4 (en,in0,in1,out0,out1,out2,out3);
+input in0,in1,en;
 output out0,out1,out2,out3;
 
-assign out0 = (~in1)&(~in0);
-assign out1 = (~in1)&(in0);
-assign out2 = (in1)&(~in0);
-assign out3 = (in1)&(in0);
+  assign out0 = en ? ((~in1)&(~in0)) : 0;
+  assign out1 = en ? ((~in1)&(in0)) : 0;
+  assign out2 = en ? ((in1)&(~in0)) : 0;
+  assign out3 = en ? ((in1)&(in0)) : 0;
 
 endmodule
